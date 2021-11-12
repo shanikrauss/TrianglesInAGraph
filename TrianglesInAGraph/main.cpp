4fc35b78aff3de9a7c2673ee31b0dd43e8e85728
shanikrauss <70583407+shanikrauss@users.noreply.github.com>
2021-11-12 12:37:18 +0200

    change main write to files

--- TrianglesInAGraph/main.cpp
+++ TrianglesInAGraph/main.cpp
@@ -2,6 +2,8 @@
 #include "AdjacencyList.h"
 #include "AdjacencyMatrix.h"
 #include <iostream>
+#include <istream>
+#include <fstream>
 
 using namespace std;
 
@@ -94,8 +96,132 @@ list<int>* alg1Inside(AdjacencyList& G, bool onlySmalldegre)
 	}
 }
 
-void main() {
+void main(int argc, char* argv[]) {
 
+	string fileName = argv[0]; 
+	
+	ifstream infile(fileName);
+	if (!infile)
+	{
+		cout << "invalid input." << endl;
+		exit(1);
+	}
+
+	char ch;
+	//file >> noskipws; // avoid skipping spaces
+	infile >> ch;
+	int numOfAlgo = ch - '0';
+
+	AdjacencyList* G = nullptr;
+	AdjacencyMatrix* G2 = nullptr;
+	
+
+	infile >> ch;
+	int numOfVert = ch - '0';
+
+	if (numOfAlgo == 1 || numOfAlgo == 3 || numOfAlgo == 4)
+	{
+		G = new AdjacencyList(numOfVert);
+	}
+	else if (numOfAlgo == 2)
+	{
+		G2 = new AdjacencyMatrix(numOfVert);
+	}
+	else
+	{
+		cout << "invalid input." << endl;
+		infile.close();
+		exit(1);
+	}
+
+	int vert, neigh;
+	while (!infile.eof())
+	{
+		if (!infile.good())
+		{
+			cout << "invalid input." << endl;
+			infile.close();
+			exit(1);
+		}
+
+		infile >> vert >> neigh;
+		//check func
+		if (numOfAlgo == 1 || numOfAlgo == 3 || numOfAlgo == 4)
+		{
+			G->addEdge(vert, neigh);
+		}
+		else
+		{
+			G2->addEdge(vert, neigh);
+		}
+	}
+
+	infile.close();
+
+
+	string fileNameRes = argv[1];
+
+	ofstream infileRes(fileNameRes);
+	if (!infileRes)
+	{
+		cout << "invalid input." << endl;
+		exit(1);
+	}
+
+	switch (numOfAlgo)
+	{
+	case 1:
+		list<int>*adj  = alg1Outside(*G);
+		infileRes << "Algo 1 res:" << endl;
+		for (auto it = adj->begin(); it != adj->end(); it++)
+		{
+			infileRes << *it << ", ";
+		}
+
+
+
+	case 2:
+		list<int>*adj = alg2(*G2);
+		infileRes << "Algo 2 res:" << endl;
+		for (auto it = adj->begin(); it != adj->end(); it++)
+		{
+			infileRes << *it << ", ";
+		}
+	case 3:
+		//list<int>*adj = alg3(*G);
+		infileRes << "Algo 3 res:" << endl;
+		for (auto it = adj->begin(); it != adj->end(); it++)
+		{
+			infileRes << *it << ", ";
+		}
+	case 4:
+		list<int>*adj1 = alg1Outside(*G);
+		list<int>* adj2 = alg2(*G2);
+		//list<int>*adj3 = alg3(*G);
+		infileRes << "Algo 1 res:" << endl;
+		for (auto it = adj1->begin(); it != adj1->end(); it++)
+		{
+			infileRes << *it << ", ";
+		}
+		infileRes << "Algo 2 res:" << endl;
+		for (auto it = adj2->begin(); it != adj2->end(); it++)
+		{
+			infileRes << *it << ", ";
+		}
+		infileRes << "Algo 3 res:" << endl;
+		//for (auto it = adj3->begin(); it != adj3->end(); it++)
+		//{
+		//	infileRes << *it << ", ";
+		//}
+
+
+	default:
+		break;
+	}
+
+
+	infileRes << "hiiii";
+	
 
 	/*/////////////////////////////////////////////////////////////////////////////
 	Arbel up
